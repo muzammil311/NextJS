bd61fc5a16b8911276fa5fe0cc8a390095a11c53
Muzammil <muzammil.z@retailinsight.io>
2023-01-12 10:04:48 +0500

    First Next JS application that contained three static pages

---
+++ app/1st/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+    return (
+            <div>my First Route</div>
+    )
+}
---
+++ app/2nd/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+    return (
+            <div>my Second Route</div>
+    )
+}
